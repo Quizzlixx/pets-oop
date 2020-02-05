07dbbda9c05c2e31b77061fbf89758790bd0ca20
Kerrie Low <klow4@mail.greenriver.edu>
2020-02-05 14:13:50 -0500

    autoload includes classes directory. Index calls class objects. default route displays class functions

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * Kerrie Low
  * 2.5.20
  * Full Stack Software Development
+ * https://klow.greenriverdev.com/328/pets-oop/
  */
 
 // turn on error reporting
@@ -12,15 +13,24 @@ error_reporting(E_ALL);
 // require the autoload file
 require_once('vendor/autoload.php');
 
+// comes after auto load
+session_start();
+
 // instantiate F3
 // :: means static method
 $f3 = Base::instance();
 
-// define a default route
-// -> calls an instance method
-$f3->route('GET /', function() {
+// default route
+$f3->route('GET /', function($f3) {
+    $pet1 = new Pet("Fido", "pink");
+    $pet2 = new Pet();
+
+    $f3->set('pet1', $pet1);
+    $f3->set('pet2', $pet2);
+
+
     $view = new Template();
-    echo $view->render('views/my-pets.html');
+    echo $view->render('views/my-pets.php');
 });
 
 // run f3
